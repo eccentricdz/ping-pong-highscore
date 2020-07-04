2fccfed4cb6c04825fbf0bcc659477d4f7c59a2d
Rahul Agarwal <simplyrahul93@gmail.com>
2020-07-04 15:20:02 +0530

    Simple server for retrieving and updating the high score

---
+++ app.js
@@ -0,0 +1,50 @@
+var express = require("express");
+var app = express();
+var port = 4000;
+
+var Datastore = require("nedb");
+db = new Datastore({ filename: "highscore" });
+db.loadDatabase();
+
+app.get("/", (req, res) => {
+  res.send(
+    "A simple service to store and retrieve high for my ping-pong web app"
+  );
+});
+
+app.get("/getHighScore", (req, res) => {
+  db.find({ type: "highscore" }, (err, docs) => {
+    if (err) res.send(err);
+    else {
+      if (docs[0]) {
+        const { name, score } = docs[0];
+        res.send({ name, score });
+      }
+      else res.send("Could not find the high score :(");
+    }
+  });
+});
+
+app.post("/updateHighScore", (req, res) => {
+  const { name, score } = req.query;
+  if (name && score) {
+    db.find({ type: "highscore" }, (err, docs) => {
+      if (docs[0])
+        db.update(
+          { type: "highscore" },
+          { name, score, type: "highscore" },
+          {}
+        );
+      else {
+        db.insert({ name, score, type: "highscore" });
+      }
+    });
+    res.send("Successfully update high score!");
+  } else {
+    res.send("Could not update the highscore :(");
+  }
+});
+
+app.listen(port, () => {
+  console.log("Server listening on port " + port);
+});
